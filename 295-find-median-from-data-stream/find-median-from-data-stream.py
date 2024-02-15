b3f4edccc87c0c7e5e0ca86d0d5947e3542d25c0
Saketh Vangala <64423964+SakethVangala11@users.noreply.github.com>
2024-02-15 17:13:24 -0500

    Time: 373 ms (67.61%) | Memory: 38 MB (93.72%) - LeetSync

--- 295-find-median-from-data-stream/find-median-from-data-stream.py
+++ 295-find-median-from-data-stream/find-median-from-data-stream.py
@@ -5,11 +5,14 @@ class MedianFinder:
         self.small,self.large = [], []
         
     def addNum(self, num: int) -> None:
+        #Push any element directly into small first
         heappush(self.small,-1*num)
+        #Make sure that small array is small than large
         if self.small and self.large:
             if -1*self.small[0]>(self.large[0]):
                 x = heappop(self.small)
                 heappush(self.large,-1*x)
+        #Make sure length of two arrays is almost equal
         if abs(len(self.small)-len(self.large))>1:
             if len(self.large)>len(self.small):
                 x = heappop(self.large)
@@ -20,10 +23,13 @@ class MedianFinder:
         
         
     def findMedian(self) -> float:
+        #If both are equal length then small array max ele and large array min ele will be median
         if len(self.small)==len(self.large):
             return ((-1*self.small[0])+self.large[0])/2
+        #Small array max ele is median
         elif len(self.small)>len(self.large):
             return -1*self.small[0]
+        #large array min ele is median
         else:
             return self.large[0]
         
