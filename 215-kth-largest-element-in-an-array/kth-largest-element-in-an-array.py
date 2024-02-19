ae7a1b0749c37b12744dc4f8254c2b23030c6cdd
Saketh Vangala <64423964+SakethVangala11@users.noreply.github.com>
2024-02-19 11:04:33 -0500

    Time: 475 ms (61.38%) | Memory: 29.9 MB (61.10%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
@@ -1,12 +1,31 @@
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        pq= []
-        for i in nums:
-            heapq.heappush(pq,-i)
-        while(k):
-            x = heapq.heappop(pq)
-            k-=1
-        return -x
+        #Heap Solution
+        # pq= []
+        # for i in nums:
+        #     heapq.heappush(pq,-i)
+        # while(k):
+        #     x = heapq.heappop(pq)
+        #     k-=1
+        # return -x
+
+        #QuickSelect solution
+        # k=len(nums)-k
+        # def quickSelect(l,r):
+        #     p, pivot = l, nums[r]
+        #     for i in range(l,r):
+        #         if nums[i]<=pivot:
+        #             nums[p], nums[i] = nums[i], nums[p]
+        #             p+=1
+        #     nums[p], nums[r] = nums[r], nums[p]
+        #     if p<k: return quickSelect(p+1,r)
+        #     elif p>k: return quickSelect(l,p-1)
+        #     else: return nums[p]
+        # return quickSelect(0,len(nums)-1)
+
+        #Sorting solution
+        nums.sort()
+        return nums[len(nums)-k]
 
 
         
