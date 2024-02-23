45c373469e7f934c19020258bfec847ac0a74c1b
Saketh Vangala <64423964+SakethVangala11@users.noreply.github.com>
2024-02-23 14:41:40 -0500

    Time: 45 ms (29.99%) | Memory: 16.7 MB (86.69%) - LeetSync

---
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.py
@@ -0,0 +1,14 @@
+class Solution:
+    def uniqueOccurrences(self, arr: List[int]) -> bool:
+        d = {}
+        for i in arr:
+            if i in d:
+                d[i]+=1
+            else:
+                d[i]=1
+        l = list(d.values())
+        n = len(l)
+        l =set(l)
+        if n == len(l):
+            return True
+        return False
