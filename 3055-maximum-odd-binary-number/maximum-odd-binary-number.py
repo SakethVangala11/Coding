0810d2882b74c62f690a44d6306af673069b8906
Saketh Vangala <64423964+SakethVangala11@users.noreply.github.com>
2024-03-01 11:40:05 -0500

    Time: 40 ms (45.47%) | Memory: 16.6 MB (75.75%) - LeetSync

--- 3055-maximum-odd-binary-number/maximum-odd-binary-number.py
+++ 3055-maximum-odd-binary-number/maximum-odd-binary-number.py
@@ -5,15 +5,7 @@ class Solution:
             if i == '1':
                 setBits+=1
         unSetBits = len(s)-setBits
-        ans = ''
-        while(setBits-1):
-            ans+='1'
-            setBits-=1
-        while(unSetBits):
-            ans+='0'
-            unSetBits-=1
-        ans+='1'
-        return ans
+        return '1'*(setBits-1) + '0'*unSetBits + '1'
 
 
         
