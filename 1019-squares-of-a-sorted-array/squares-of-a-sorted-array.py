dcb894854d1cca89916b122c9deb0fa08688c553
Saketh Vangala <64423964+SakethVangala11@users.noreply.github.com>
2024-03-02 12:01:03 -0500

    Time: 178 ms (11.20%) | Memory: 18.7 MB (72.31%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.py
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.py
@@ -1,12 +1,17 @@
 import heapq
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
-        result = []
-        for i in nums:
-            heapq.heappush(result, i*i)
-        ans = []
-        while(result):
-            x = heapq.heappop(result)
-            ans.append(x)
-        return ans
+        result = [None]*len(nums)
+        i = 0
+        j = len(nums)-1
+        for idx in range(len(nums)-1,-1,-1):
+            if abs(nums[i]) > abs(nums[j]):
+                result[idx] = nums[i]**2
+                i+=1
+            else:
+                result[idx] = nums[j]**2
+                j-=1
+        return result
+
+
         
