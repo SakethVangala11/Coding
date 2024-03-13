5911dd757f7e698e688bfba4d7d0df5a8d3e9241
Saketh Vangala <64423964+SakethVangala11@users.noreply.github.com>
2024-03-13 12:40:17 -0400

    Time: 38 ms (85.89%) | Memory: 16.6 MB (23.99%) - LeetSync

--- 2571-find-the-pivot-integer/find-the-pivot-integer.py
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.py
@@ -1,12 +1,8 @@
 class Solution:
     def pivotInteger(self, n: int) -> int:
-        if n==1:
-            return 1
-        sumo = (n*(n+1))//2
-        for i in range(n):
-            cur = (i*(i+1))//2
-            if cur == sumo-cur+i:
-                return i
+        x = math.sqrt((n*(n+1))/2)
+        if x.is_integer():
+            return int(x)
         return -1
 
         
