052bd5648b3f232ac623e5522f5b9ab0dcf30cc8
Saketh Vangala <64423964+SakethVangala11@users.noreply.github.com>
2024-03-18 20:41:18 -0400

    Time: 29 ms (90.87%) | Memory: 16.6 MB (61.71%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.py
+++ 54-spiral-matrix/spiral-matrix.py
@@ -5,23 +5,28 @@ class Solution:
         res  = []
 
         while(left<right and top<bottom):
+
+            #Add elements from left to right in top row
             for i in range(left,right):
                 res.append(matrix[top][i])
             top+=1
 
-            
-
+            #Add elements from top to bottom in right-most row
             for i in range(top,bottom):
                 res.append(matrix[i][right-1])
             right-=1
 
+            #For column matrix or row matrix or if one row or column in sub problems
             if not(left<right and top<bottom):
                 break
-
+            
+            #Add elements from right to left in bottom-most row
+            
             for i in range(right-1,left-1,-1):
                 res.append(matrix[bottom-1][i])
             bottom-=1
-
+            
+            #Add elements from bottom to top in left row
             for i in range(bottom-1,top-1,-1):
                 res.append(matrix[i][left])
             left+=1
