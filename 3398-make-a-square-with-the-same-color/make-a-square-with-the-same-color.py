af160125fc12c469d72c40ab8add7bc7eb130618
Saketh Vangala <64423964+SakethVangala11@users.noreply.github.com>
2024-04-28 15:28:01 -0400

    Time: 40 ms (100.00%) | Memory: 16.6 MB (50.00%) - LeetSync

--- 3398-make-a-square-with-the-same-color/make-a-square-with-the-same-color.py
+++ 3398-make-a-square-with-the-same-color/make-a-square-with-the-same-color.py
@@ -2,24 +2,14 @@ class Solution:
     def canMakeSquare(self, grid: List[List[str]]) -> bool:
         for i in range(2):
             for j in range(2):
+                pos = [grid[i][j], grid[i+1][j], grid[i][j+1], grid[i+1][j+1]]
                 count_W = 0
                 count_B = 0
-                if grid[i][j] == 'W':
-                    count_W+=1
-                else:
-                    count_B+=1
-                if grid[i+1][j] == 'W':
-                    count_W+=1
-                else:
-                    count_B+=1
-                if grid[i][j+1] == 'W':
-                    count_W+=1
-                else:
-                    count_B+=1
-                if grid[i+1][j+1] == 'W':
-                    count_W+=1
-                else:
-                    count_B+=1
+                for k in range(len(pos)):
+                    if pos[k] == 'W':
+                        count_W+=1
+                    else:
+                        count_B+=1
                 if count_W >= 3 or count_B >= 3:
                     return True
         return False
