bb069db2aa8ecbcf2c2f73cdf84adefeb802f688
Saketh Vangala <64423964+SakethVangala11@users.noreply.github.com>
2024-07-16 22:41:32 -0400

    Time: 3 ms (47.45%) | Memory: 43 MB (30.11%) - LeetSync

---
+++ 2892-check-if-array-is-good/check-if-array-is-good.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isGood(int[] nums) {
+        int n=nums.length;
+        Arrays.sort(nums);
+        for(int i = 0; i<n-1;i++){
+            if(i+1 != nums[i]){
+                return false;
+            }
+        }
+        if(nums[n-1]!=n-1){
+            return false;
+        }
+       return true; 
+    }
+}
