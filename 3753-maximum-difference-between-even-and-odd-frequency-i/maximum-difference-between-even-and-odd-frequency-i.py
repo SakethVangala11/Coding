f2db9e7072a318ecf49f9f8bc873acd2547fe58d
Saketh Vangala <64423964+SakethVangala11@users.noreply.github.com>
2025-02-08 16:52:13 -0500

    Time: 5 ms (24.20%) | Memory: 18 MB (31.09%) - LeetSync

--- 3753-maximum-difference-between-even-and-odd-frequency-i/maximum-difference-between-even-and-odd-frequency-i.py
+++ 3753-maximum-difference-between-even-and-odd-frequency-i/maximum-difference-between-even-and-odd-frequency-i.py
@@ -6,16 +6,12 @@ class Solution:
                 d[i]+=1
             else:
                 d[i]=1
-        min_odd, max_odd, min_even, max_even = 150,0,150,0
+        max_odd, min_even = 0,150
         for i in d:
             if d[i]%2==0:
-                if d[i] > max_even:
-                    max_even = d[i]
                 if d[i] < min_even:
                     min_even = d[i]
             else:
                 if d[i] > max_odd:
                     max_odd = d[i]
-                if d[i] < min_odd:
-                    min_odd = d[i]
         return max_odd-min_even
