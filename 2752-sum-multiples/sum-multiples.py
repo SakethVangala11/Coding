03af6992e189659749dd5e31586914b6dd526a12
Saketh Vangala <64423964+SakethVangala11@users.noreply.github.com>
2025-03-22 23:36:38 -0400

    Time: 65 ms (5.27%) | Memory: 18 MB (24.62%) - LeetSync

--- 2752-sum-multiples/sum-multiples.py
+++ 2752-sum-multiples/sum-multiples.py
@@ -1,6 +1,7 @@
 class Solution:
     def sumOfMultiples(self, n: int) -> int:
         arr = [False]*1001
+        print(len(arr))
         for i in range(1, len(arr)):
             if i%3==0 or i%5==0 or i%7==0:
                 arr[i]=True
